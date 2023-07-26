f26966c09dace94c8fb27d7141e11822fe0a9387
EastburnJim <jeastburn@cargurus.com>
2023-07-26 16:24:38 -0500

    First commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,37 @@
 import '../styles/globals.css'
 
+import useSWR from 'swr'
+
+async function fetcher(url){
+  const res = await fetch(url);
+  return await res.json();
+}
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+  const { data:gPT3data, error:gPT3error, isLoading:gPT3IsLoading } = useSWR(['/api/gpt3'], fetcher);
+  const { data:autolistData, error:autolistError, isLoading:autolistIsLoading } = useSWR(() => (gPT3data ? 
+    `https://www.autolist.com/api/v2/search?page=1
+    &make=${gPT3data.answer.make}
+    &model=${gPT3data.answer.model}
+    &year_max=${gPT3data.answer.year}
+    &year_min=${gPT3data.answer.year}` : null), fetcher);
+
+  if (gPT3error) return <div>Failed to load GPT3 data</div>
+  if (gPT3IsLoading) return <div>Loading GPT3 data...</div>
+  if (!gPT3data) return null
+
+  
+
+  if (autolistError) return <div>Failed to load Autolist data</div>
+  if (autolistIsLoading) return <div>Loading Autolist data...</div>
+  if (!autolistData) return null
+
+  return (
+    <div>
+      {autolistData.records[0].make_and_model}
+    </div>
+  )
 }
 
 export default MyApp
---
+++ pages/api/gpt3.js
@@ -0,0 +1,26 @@
+import { Configuration, OpenAIApi } from "openai";
+
+export default async function handler(req, res) {
+  const configuration = new Configuration({
+    apiKey: "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",
+  });
+
+  const openai = new OpenAIApi(configuration);
+
+  const search = "looking for a 2019 camry in New Jersey";
+
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: 'user',
+        content: 'Output only JSON - parse this response in format `year: `,`make: `, `model: `, `trim: `, `body_style: suv, sedan, or truck`, `location: city,  ST` as a json: ${search}'
+      }
+    ],
+    temperature: 0,
+    max_tokens: 500,
+  });
+
+  res.status(200).json({ answer: JSON.parse(response.data.choices[0].message.content) });
+
+}

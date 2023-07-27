c6caca73a17bf5f41806c4fee0a692c32f03c279
EastburnJim <jeastburn@cargurus.com>
2023-07-27 15:19:20 -0500

    style changes

--- pages/api/gpt3.js
+++ pages/api/gpt3.js
@@ -1,20 +1,23 @@
 import { Configuration, OpenAIApi } from "openai";
 
 export default async function handler(req, res) {
+
+  console.log("openAIhandler");
   const configuration = new Configuration({
     apiKey: process.env.OPEN_AI_API_KEY
   });
 
   const openai = new OpenAIApi(configuration);
 
-  const search = "looking for a 2019 camry in New Jersey";
+  //const searchSentence = "Im looking for a 2019 Camry in New Jersey";
+  const searchSentence = req.query.searchSentence;
 
   const response = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     messages: [
       {
         role: 'user',
-        content: 'Output only JSON - parse this response in format `year: `,`make: `, `model: `, `trim: `, `body_style: suv, sedan, or truck`, `location: city,  ST` as a json: ${search}'
+        content: 'Output only JSON - parse this response in format `year: `,`make: `, `model: `, `trim: `, `body_style: suv, sedan, or truck`, `location: city,  ST` as a json: ${searchSentence}'
       }
     ],
     temperature: 0,
